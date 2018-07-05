5fd787b3ae0b7296a9a910f7f9880f1f776700ea
Mathias Battistella <mathias2b@gmail.com>
2018-07-05 06:52:55 +0100

    2011 fin con duda

--- 2011/SpecialIterator.h
+++ 2011/SpecialIterator.h
@@ -8,12 +8,12 @@ class SpecialIterator : public Iterator{
 		bool forward;
 		SpecialIterator();
 	public:
-		virtual bool hasNext(); ///¿POR QUÉ ES VIRTUAL?
-		virtual Object* next(); ///¿POR QUÉ ES VIRTUAL?
+		virtual bool hasNext(); ///¿POR QUÉ ES VIRTUAL? *
+		virtual Object* next(); ///¿POR QUÉ ES VIRTUAL? *
 		void setForward();
 		void setBackward();
 		void setStepSize(int);
 
 		SpecialIterator(Node* n, int size);
-		virtual ~SpecialIterator(); ///¿POR QUÉ ES VIRTUAL?
+		virtual ~SpecialIterator(); ///¿POR QUÉ ES VIRTUAL? *
 }
