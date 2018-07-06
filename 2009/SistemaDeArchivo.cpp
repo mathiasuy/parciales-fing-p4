60cc0c462f8c7ac6dfe4c1f0e4683e39fec1c587
Mathias Battistella <mathias2b@gmail.com>
2018-07-07 00:44:59 +0100

    2009, uso de IDictionary, IKey estatico y con memoria

--- 2009/SistemaDeArchivo.cpp
+++ 2009/SistemaDeArchivo.cpp
@@ -1,11 +1,23 @@
 	SistemaDeArchivo* SistemaDeArchivo::instance = NULL;
 
 	void SistemaDeArchivo::crearArchivo(int idCarpeta,int idArchivo,int tamanio){
+//  VERSIÓN ALTERNATIVA DE KEY ESTATICA (NO PRECISA LIBERAR MEMORIA)
+		/* VERSION 1 */
 		KeyInteger ski(idCarpeta);
-		Directorio* directorio = (Directorio*)elementos->find(&ski); //  NOTAR QUE USE VERSIÓN ALTERNATIVA DE KEY ESTATICA (NO PRECISA LIBERAR MEMORIA)
+		Directorio* directorio = (Directorio*)elementos->find(&ski); //NOTAR EL &
+		/* FIN VERSION 1 */
+
+//  VERSIÓN ALTERNATIVA QUE CREA MEMORIA (LUEGO HAY QUE LIBERARLA)
+		/* VERSION 2 */
+		KeyInteger key = new KeyInteger(idCarpeta);
+		Directorio* directorio = (Directorio*)elementos->find(key);
+		delete key; // BORRO LA KEY
+		/* FIN VERSION 2 */
+
 		if (directorio != NULL){
 			Archivo* archivo = directorio->crearArchivo(idArchivo,tamanio);
-			this->elementos->add(new KeyInteger(idArchivo),archivo); // AL AGREGAR LA KEY A LA COLECCION DEBO CREARLE MEMORIA
+			// AL AGREGAR LA KEY A LA COLECCION DEBO CREARLE MEMORIA
+			this->elementos->add(new KeyInteger(idArchivo),archivo);
 		}else
 			throw std::invalid_argument("Directorio no valido");
 	};
